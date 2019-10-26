6caea08782ea78631ef31ed86ed735dcd9fe3748
Lao <lao@nexverse.net>
2019-10-26 04:21:54 -0600

    Change how we trap / wait for close

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,16 @@
 
 # USE the trap if you need to also do manual cleanup after the service is stopped,
 #     or need to start multiple services in the one container
-trap "echo TRAPed signal" HUP INT QUIT TERM
+trap stop INT
+trap stop TERM
 
 # TODO: Start server here
 
 # TODO: Ensure cron is running
 
 echo "[hit enter key to exit] or run 'docker stop <container>'"
-read
+read < /tmp/FIFO &
+wait
 
 # stop service and clean up here
 echo "Stopping Atlas"
